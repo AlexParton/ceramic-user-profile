9cc46d1f2f4d47c80090ef22af2ae87ae3b1157a
AlexParton <43645696+AlexParton@users.noreply.github.com>
2023-01-06 12:00:51 +0100

    practice finished

---
+++ frontend/pages/_app.js
@@ -0,0 +1,9 @@
+import { Provider } from "@self.id/react";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
+}
